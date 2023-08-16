82f83b5d83d70222d6b1d992196a1b1174d3907b
phoenixsolutions <phoenixsolutions.org@gmail.com>
2023-08-16 04:28:23 -0700

    Incomplete code

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import ProductTable from "../ProductTable/ProductTable";
+import Sidenav from "../Sidenav/Sidenav";
+import Topnav from "../Topnavbar/Topnav";
+
+export default function Home(){
+    return<>
+    
+    <Topnav/>
+    <Sidenav/>
+    <ProductTable/>
+    </>
+}
